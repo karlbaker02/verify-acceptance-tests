32b895aaabc9dab537e8be224c004cded342e1e4
Anshul Sirur <anshul.sirur@digital.cabinet-office.gov.uk>
2018-05-24 14:50:05 +0100

    Run using headless Geckodriver
    Author: @vixus0

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,5 +1,15 @@
 require 'capybara/cucumber'
+require 'selenium/webdriver'
 
 Capybara.configure do |cfg|
   cfg.default_max_wait_time = 10
 end
+
+Capybara.register_driver :firefox_headless do |app|
+  options = ::Selenium::WebDriver::Firefox::Options.new
+  options.args << '--headless'
+
+  Capybara::Selenium::Driver.new(app, browser: :firefox, options: options)
+end
+
+Capybara.javascript_driver = :firefox_headless
