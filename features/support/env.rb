e50e9f77bbcad55179ecff513cde43a9be489ddb
Andy Paine <andy.paine@digital.cabinet-office.gov.uk>
2018-06-05 10:58:44 +0100

    BAU: Allow local running of tests
    - Local startup still needs some work to allow the federation
    to be acceptance tested in docker
    - The only way to spin up the services at the moment is using
    Java processes from ida-hub-acceptance tests
    - The docker image cannot access "localhost" from the host machine
    so we need to be able to run tests on the host machine too
    - This adds back the previously used gecko/firefox browser driver
    which is switched by setting TEST_ENV to "local"
    - Also restores the SHOW_BROWSER env var to control whether to use
    a real firefox browser
    
    Co-authored-by: Chris Grimble <chris.grimble@digital.cabinet-office.gov.uk>
    Co-authored-by: James Howes <james.howes@digital.cabinet-office.gov.uk>

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,10 +5,18 @@ Capybara.configure do |cfg|
   cfg.default_max_wait_time = 20
 end
 
-Capybara.register_driver "selenium_remote_firefox".to_sym do |app|
-  Capybara::Selenium::Driver.new(app, browser: :remote, url: "http://selenium-hub:4444/wd/hub", desired_capabilities: :firefox)
-end
+if ENV['TEST_ENV'] == "local"
+  Capybara.register_driver :firefox_headless do |app|
+    options = ::Selenium::WebDriver::Firefox::Options.new
+    options.args << '--headless'
 
-unless ENV['SHOW_BROWSER'] == 'true'
+    Capybara::Selenium::Driver.new(app, browser: :firefox, options: options)
+  end
+  Capybara.javascript_driver = :firefox_headless unless ENV['SHOW_BROWSER'] == 'true'
+else
+  Capybara.register_driver "selenium_remote_firefox".to_sym do |app|
+    Capybara::Selenium::Driver.new(app, browser: :remote, url: "http://selenium-hub:4444/wd/hub", desired_capabilities: :firefox)
+  end
+
   Capybara.javascript_driver = :selenium_remote_firefox
 end
