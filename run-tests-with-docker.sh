c43aa3786244807cfc4ee1d9a95e51ff850e1610
Andy Paine <andy.paine@digital.cabinet-office.gov.uk>
2018-07-04 13:28:51 +0100

    BAU: Add eIDAS tags to test and improve run-docker script
    - Currently eIDAS is not enabled on staging so we need a way
    to not run these tests when running against staging
    - Added @Eidas annotation to relevant tests
    - Quoted $@ to allow passing of string parameters in bash
    - Add explicit exit_code to shell script in case docker cp fails

--- run-tests-with-docker.sh
+++ run-tests-with-docker.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env sh
-set -eu
-
-teardown() {
-    docker cp $(docker ps -a -q -f name="test-runner"):/testreport .
-    docker-compose down
-}
-
-trap teardown EXIT
+set -u
 
 docker-compose build
 docker-compose run \
                -e TEST_ENV=${TEST_ENV:-"joint"} \
-               test-runner -f pretty -f junit -o testreport/ $@
+               test-runner -f pretty -f junit -o testreport/ "$@"
+exit_status=$?
+docker cp $(docker ps -a -q -f name="test-runner"):/testreport .
+docker-compose down
+exit $exit_status
